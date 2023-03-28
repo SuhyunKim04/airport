79e6eaf259711cedd79662c63a249faf16cfa19d
Suhyun Kim <kim00431@algonquinlive.com>
2023-03-28 15:08:04 +0900

    20230328

---
+++ js/test.js
@@ -0,0 +1,14 @@
+const arrys = [ 1, 3, 8, 10, 2];
+const newArrays = []
+arrys.forEach(function(arry){
+    newArrays.push(arry + 1); 
+})
+
+console.log(arrys, newArrays)
+
+
+
+
+// items.forEach(function(item){
+//     copy.push(item);
+//   });

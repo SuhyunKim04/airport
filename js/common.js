d8c339671f9c35f3f350c9830328f26be8cff81b
Suhyun Kim <kim00431@algonquinlive.com>
2023-04-09 08:44:02 +0900

    modal update

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('.openModal');
 const dimm = document.querySelector('.dimm');
-const view = document.querySelector('.view_detail');
+// const view = document.querySelector('.view_detail');
 button.addEventListener('click',(e)=>{
     view.classList.add('open');
     dimm.classList.add('open');
@@ -9,35 +9,72 @@ button.addEventListener('click',(e)=>{
 const datas = [
     {
         id:0,
-        title:'1월',
-        journey: `<p>
-                    <strong>서울 / 인천</strong>
-                    <i class="material-symbols-outlined">
-                        sync_alt
-                    </i>
-                    <strong>오사카</strong>
-                </p> `,
-        information:`<span>왕복</span> 
-                        <p>
-                            <b>KRW</b>
-                            <em>1,482,500</em>
-                            <span>~</span>
-                        </p>`
-    }
+        month: 1,
+        price: 1482500,
+        information: '1월입니다.'
+    },
+    {
+        id:1,
+        month: 2,
+        price: 100,
+        information: '2월입니다.'
+    },
+    {
+        id:2,
+        month: 3,
+        price: 100,
+        information: '3월입니다.'
+    },
 ]
 
-const viewInfor = () => {
-    const view = document.querySelector('.recommand')
-    const card = recommand.querySelector('.module_slide_card');
 
+const viewInfor = () => {
+    const view = document.querySelector('.view_detail');
+    const list = document.querySelectorAll('.recommand .module_slide_card li');
+    const dimm = document.querySelector('.dimm');
+    const depart = view.querySelector('.departure');
+    const arrive = view.querySelector('.arrive');
+    const price = view.querySelector('.price');
+    const info = view.querySelector('.info');
+    const close = view.querySelector('.close');
+
+    const openModal = () => {
+        
+    }
+    const closeModal = () => {
+        view.classList.remove('open');
+        dimm.classList.remove('open');
+    }
+    console.log(close)
+    close.addEventListener('click', closeModal);
+ 
     function setDetail(index) {
-        const depart = view.querySelector('.departure');
-        const icon = view.querySelector('.material-symbols-outlined');
-        const arrive = view.querySelector('.arrive');
-        const way = view.querySelector('.way');
-        const exchange = view.querySelector('.exchange')
-        const price = view.querySelector('.price')
-        const symbol = view.querySelector('.symbol')
+        depart.innerHTML = datas[index].departure
+        arrive.innerHTML = datas[index].arrive
+        price.innerHTML = datas[index].price
+    }
+
+    function setjourney(index) {
 
     }
+
+    list.forEach( (e,idx) => {
+        e.addEventListener('click',(event) => {
+            const target = event.target;
+            console.log(target.innerHTML);
+            if(target.nodeName == 'A') {
+                view.classList.add('open');
+                dimm.classList.add('open');
+                info.innerHTML = target.innerHTML
+            }
+        })
+    })
+
+    
 }
+
+viewInfor();
+
+
+
+

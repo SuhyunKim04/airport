4f89a3cfd548e16816d2e07594d42aca866cc292
Suhyun Kim <kim00431@algonquinlive.com>
2023-04-08 22:41:04 +0900

    javascript update

--- js/common.js
+++ js/common.js
@@ -5,3 +5,39 @@ button.addEventListener('click',(e)=>{
     view.classList.add('open');
     dimm.classList.add('open');
 })
+
+const datas = [
+    {
+        id:0,
+        title:'1월',
+        journey: `<p>
+                    <strong>서울 / 인천</strong>
+                    <i class="material-symbols-outlined">
+                        sync_alt
+                    </i>
+                    <strong>오사카</strong>
+                </p> `,
+        information:`<span>왕복</span> 
+                        <p>
+                            <b>KRW</b>
+                            <em>1,482,500</em>
+                            <span>~</span>
+                        </p>`
+    }
+]
+
+const viewInfor = () => {
+    const view = document.querySelector('.recommand')
+    const card = recommand.querySelector('.module_slide_card');
+
+    function setDetail(index) {
+        const depart = view.querySelector('.departure');
+        const icon = view.querySelector('.material-symbols-outlined');
+        const arrive = view.querySelector('.arrive');
+        const way = view.querySelector('.way');
+        const exchange = view.querySelector('.exchange')
+        const price = view.querySelector('.price')
+        const symbol = view.querySelector('.symbol')
+
+    }
+}

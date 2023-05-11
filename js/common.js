63d0caca015865bebb740cffce747e983973fabd
Suhyun Kim <kim00431@algonquinlive.com>
2023-05-11 23:40:29 +0900

    script update

--- js/common.js
+++ js/common.js
@@ -120,6 +120,7 @@ const viewInfor = () => {
             if(target.nodeName == 'A') {
                 openModal()
                 info.innerHTML = target.innerHTML + datas[now].information;
+                info.innerHTML = target.innerHTML + datas.price;
             }
         })
     })

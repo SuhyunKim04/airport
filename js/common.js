231e0e51255a9c481cd5482fcac26fbb07fce572
Suhyun Kim <kim00431@algonquinlive.com>
2023-06-05 23:58:58 +0900

    script

--- js/common.js
+++ js/common.js
@@ -1,10 +1,3 @@
-// const button = document.querySelector('.openModal');
-// const dimm = document.querySelector('.dimm');
-// // const view = document.querySelector('.view_detail');
-// button.addEventListener('click',(e)=>{
-//     view.classList.add('open');
-//     dimm.classList.add('open');
-// })
 document.addEventListener("DOMContentLoaded", (event) => {
 
 const tripLists = [
